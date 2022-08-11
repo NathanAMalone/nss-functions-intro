108e765bdb4800052336b8b8e42e07d6bf786e99
Nathan Malone <nathanamalone@gmail.com>
2022-08-11 14:13:59 -0500

    Functions Intro complete

---
+++ main.js
@@ -0,0 +1,16 @@
+// const fillGasTank = (gallons) => {
+//     console.log(`I filled the tank with ${gallons} of gas`)
+// }
+
+// fillGasTank(25)
+
+// const fillGasTank = (gallons) => {
+//     if (gallons <= 15) {
+//         console.log(`I filled the tank with ${gallons} of gas`);
+//     }
+//         else {console.log("You cannot add this much gas to the tank.");
+//         }
+// }
+// fillGasTank(25)
+
+
